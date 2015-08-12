6a4fa5b690700bdccc89595202867cfcf143926c
Denis Doan <dedoin08@yahoo.de>
2015-08-12 17:53:08 +0100

    Calc 2

--- calc.js
+++ calc.js
@@ -11,8 +11,14 @@ alert("Why is your console not open?");
 
 var num1 =prompt("Type in your 1st number");
 var num2 =prompt("Type in your 2nd number");
-var op = prompt("Type in your basics operator")
+var op = prompt("Type in your basics operator like +, -, / or *")
+
 if (op === "+") {
   var sum = parseFloat(num1) + parseFloat(num2);
-  console.log("The sum is" + sum)
+  console.log("The sum is " + sum)
+}
+
+else if (op === "-") {
+  var diff = parseFloat(num1) - parseFloat(num2);
+  console.log("The difference is " + diff)
 }

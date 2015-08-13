3af96caa477fc7c188be9fe2efe5f58b56d38dd3
Denis Doan <dedoin08@yahoo.de>
2015-08-13 08:49:55 +0100

    Bonus

---
+++ bmi/bmi.js
@@ -0,0 +1,21 @@
+/*var op = prompt("Choose a number");
+console.log("op",op);
+
+var num = prompt("Choose a number");
+console.log("num",num);
+
+var goodNum = parseFloat(num);
+
+alert("Why is your console not open?"); 
+*/
+
+var num1 =prompt("Type in your weight in Kg");
+var num2 =prompt("Type in your length in meters")
+
+  var res = parseFloat(num1) / (parseFloat(num2)*parseFloat(num2));
+  alert("Your BMI is " + res)
+
+
+
+
+
